1d39e6a0079383bfc2fbb1d3923c14bc878ca935
Myllena000 <myllena000@hotmail.com>
2025-09-30 22:21:47 -0300

    Fazendo alteraçao pra Spring

---
+++ src/main/java/com/Myllena/Aprendendo_Spring/AprendendoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Myllena.Aprendendo_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoSpringApplication.class, args);
+	}
+
+}
